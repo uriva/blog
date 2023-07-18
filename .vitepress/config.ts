d840c74a71b937d8dc85194eebdd7aa285f408da
uri <uri.valevski@gmail.com>
2023-07-19 02:27:42 +0300

    remove examples

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,23 +7,8 @@ export default defineConfig({
   base: "/blog/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
-    ],
-
-    sidebar: [
-      {
-        text: "Examples",
-        items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
-    ],
-
-    socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
-    ],
+    nav: [{ text: "Home", link: "/" }],
+    sidebar: [],
+    socialLinks: [{ icon: "github", link: "https://github.com/uriva/" }],
   },
 });
--- index.md
+++ index.md
@@ -3,25 +3,11 @@
 layout: home
 
 hero:
-  name: "Blog"
-  text: "a blog"
-  tagline: My blog
+  name: "blog"
+  text: "blog"
+  tagline: blog
   actions:
-    - theme: brand
+    - theme: alt
       text: Graph programming
       link: /graph-programming
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
-
-features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
